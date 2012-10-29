765d10b9106e1caacf380598b5d3b10f7fc64507
Tom Xue <tomxue0126@gmail.com>
2012-10-29 18:32:50 +0800

    workable version

--- src/com/example/mycalculator/Calculator.java
+++ src/com/example/mycalculator/Calculator.java
@@ -6,11 +6,16 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Calculator extends Activity {
 
 	private int num1 = 0;
+	private int num2 = 0;
+	private float num3 = 0;
+	private int operator1 = 0;
+	private int operator2 = 0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,12 @@ public class Calculator extends Activity {
 		Button button8 = (Button) findViewById(R.id.button8);
 		Button button9 = (Button) findViewById(R.id.button9);
 		Button button_quit = (Button) findViewById(R.id.button_quit);
+		Button button_ac = (Button) findViewById(R.id.button_ac);
+		Button button_mul = (Button) findViewById(R.id.button_mul);
+		Button button_div = (Button) findViewById(R.id.button_div);
+		Button button_add = (Button) findViewById(R.id.button_add);
+		Button button_minus = (Button) findViewById(R.id.button_minus);
+		Button button_equal = (Button) findViewById(R.id.button_equal);
 
 		button0.setOnClickListener(mClickListener_button0);
 		button1.setOnClickListener(mClickListener_button1);
@@ -40,7 +51,12 @@ public class Calculator extends Activity {
 		button8.setOnClickListener(mClickListener_button8);
 		button9.setOnClickListener(mClickListener_button9);
 		button_quit.setOnClickListener(mClickListener_button_quit);
-
+		button_ac.setOnClickListener(mClickListener_button_ac);
+		button_mul.setOnClickListener(mClickListener_button_mul);
+		button_div.setOnClickListener(mClickListener_button_div);
+		button_add.setOnClickListener(mClickListener_button_add);
+		button_minus.setOnClickListener(mClickListener_button_minus);
+		button_equal.setOnClickListener(mClickListener_button_equal);
 	}
 
 	@Override
@@ -52,87 +68,260 @@ public class Calculator extends Activity {
 	private Button.OnClickListener mClickListener_button0 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 0;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 0;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 0;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button1 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 1;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 1;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 1;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button2 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 2;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 2;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 2;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button3 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 3;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 3;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 3;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button4 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 4;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 4;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 4;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button5 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 5;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 5;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 5;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button6 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 6;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 6;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 6;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button7 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 7;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 7;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 7;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button8 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 8;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 8;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 8;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button9 = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
-			num1 = num1 * 10 + 9;
-			DisplayToast(Integer.toString(num1));
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+
+			if (operator1 == 0) {
+				num1 = num1 * 10 + 9;
+				editText_result.setText(Integer.toString(num1));
+			} else {
+				num2 = num2 * 10 + 9;
+				editText_result.setText(Integer.toString(num2));
+			}
+
+			// DisplayToast(Integer.toString(num1));
 		}
 	};
 
 	private Button.OnClickListener mClickListener_button_quit = new Button.OnClickListener() {
 		@Override
 		public void onClick(View arg0) {
+			// DisplayToast("Quit the application");
 			finish();
+		}
+	};
+
+	private Button.OnClickListener mClickListener_button_ac = new Button.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			num1 = 0;
+			num2 = 0;
+			operator1 = 0;
+			operator2 = 0;
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+			editText_result.setText("0");
+
+			// DisplayToast(Integer.toString(num1));
+		}
+	};
+
+	private Button.OnClickListener mClickListener_button_mul = new Button.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			operator1 = 1;
+			// DisplayToast("multiply");
+		}
+	};
+
+	private Button.OnClickListener mClickListener_button_div = new Button.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			operator1 = 2;
+			// DisplayToast("divide");
+		}
+	};
+
+	private Button.OnClickListener mClickListener_button_add = new Button.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			operator1 = 3;
+			// DisplayToast("add");
+		}
+	};
+
+	private Button.OnClickListener mClickListener_button_minus = new Button.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			operator1 = 1;
+			// DisplayToast("minus");
+		}
+	};
+
+	private Button.OnClickListener mClickListener_button_equal = new Button.OnClickListener() {
+		@Override
+		public void onClick(View arg0) {
+			operator2 = 1;
+			if (operator1 == 1) // multiply
+				num3 = num1 * num2;
+			else if (operator1 == 2) // divide
+				num3 = (float) num1 / num2;
+			else if (operator1 == 3) // add
+				num3 = num1 + num2;
+			else if (operator1 == 4) // minus
+				num3 = num1 - num2;
+
+			EditText editText_result = (EditText) findViewById(R.id.editText_result);
+			editText_result.setText(Float.toString(num3));
+
+			num1 = 0;
+			num2 = 0;
+			operator1 = 0;
+			operator2 = 0;
+
+			// DisplayToast(Float.toString(num3));
 		}
 	};
 
